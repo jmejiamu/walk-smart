27fc52bbcf7ef278203f5d60a4013a78b71479e1
jmejiamu <jmejiamu@gmail.com>
2023-10-15 21:13:40 -0700

    feat(navigation): setting up navigation #33

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import StorybookUIRoot from './.storybook';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 // TODO: change this to 'false' so you have access to the main app.
 const SBLOADER = 'true';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>App</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <View>
+          <Text>App</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
