48e041fed323960f92d9ee51d1926abf2e3d30ff
redmejia <reynaldoemejia@gmail.com>
2023-11-25 16:00:12 -0800

    all event list

--- src/screens/EventsScreen/EventsScreen.tsx
+++ src/screens/EventsScreen/EventsScreen.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { View, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useContext, useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, TouchableOpacity, View } from 'react-native';
 import ListCard from '../../../.storybook/stories/ListCard/ListCard';
 import Search from '../../../.storybook/stories/Search/Search';
-import { Event, Events } from '../../interface/models';
-import { useFetch } from '../../hook/useFetch';
-import { useNavigation } from '@react-navigation/native';
+import { EventCtx } from '../../Context/EventContext';
 import { Navigation } from '../../Main/Main';
 import useDateFormat from '../../hook/useDateFormat';
+import { Event } from '../../interface/models';
 
 interface SearchProps extends Event {
 	searchEvent: string;
 }
 
-const CardEventRender = ({ event_title,  time_stamp = '', event_id }: Event) => {
+const CardEventRender = ({ event_title, time_stamp = '', event_id }: Event) => {
 	const { formater } = useDateFormat()
-	
+
 	const nav = useNavigation<Navigation>()
 
 	return (
 		<View>
 			<TouchableOpacity activeOpacity={0.7} onPress={() => nav.navigate('Event', { event_id })}>
 				<View style={{ marginTop: 10 }} />
-				<ListCard key={event_id}  EventName={event_title} Date={formater(time_stamp, { month: 'short', day: 'numeric', year: 'numeric', hour:'2-digit' })} />
+				<ListCard key={event_id} EventName={event_title} Date={formater(time_stamp, { month: 'short', day: 'numeric', year: 'numeric', hour: '2-digit' })} />
 			</TouchableOpacity>
 		</View>
 	)
@@ -43,14 +43,10 @@ const SearchEventRender = ({ searchEvent, event_title, time_stamp, event_id }: S
 const EventsScreen = () => {
 
 	const [search, setSearch] = useState("")
-	const { data, fetcheer } = useFetch<Events>({
-		error: true,
-		message: '',
-		events: []
-	})
+	const { events, getAllEvents } = useContext(EventCtx)
 
 	useEffect(() => {
-		fetcheer('http://localhost:8080/api-v1/events/all')
+		getAllEvents()
 	}, [])
 
 	return (
@@ -62,7 +58,7 @@ const EventsScreen = () => {
 
 					search.length > 0 ?
 						<FlatList
-							data={data.events}
+							data={events.events}
 							showsVerticalScrollIndicator={false}
 							renderItem={({ item }) => <SearchEventRender
 								event_id={item.event_id}
@@ -74,7 +70,7 @@ const EventsScreen = () => {
 						/>
 						:
 						<FlatList
-							data={data.events}
+							data={events.events}
 							showsVerticalScrollIndicator={false}
 							renderItem={({ item }) => <CardEventRender
 								event_id={item.event_id}
