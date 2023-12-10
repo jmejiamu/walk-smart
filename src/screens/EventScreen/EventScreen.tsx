d5ab4aff931b99acd59b4ce7da46c9226b13b2f9
redmejia <reynaldoemejia@gmail.com>
2023-12-09 17:05:10 -0800

    reposive event screen

--- src/screens/EventScreen/EventScreen.tsx
+++ src/screens/EventScreen/EventScreen.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Dimensions, ScrollView, Text, View } from "react-native"
+import { Dimensions, Platform, ScrollView, Text, View } from "react-native"
 import { RouteProp, useRoute } from "@react-navigation/native";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { colors } from "../../theme";
@@ -8,9 +8,7 @@ import { RootStacksParams } from "../../Main/Main";
 import EventHeader from "../../../.storybook/stories/EventHeader/EventHeader";
 import ButtonComponent from "../../../.storybook/stories/Button/Button";
 import { EventCtx } from "../../Context/EventContext";
-// import { Event, EventInfo } from "../../interface/models";
 import Alert from "../../../.storybook/stories/Alert/Alert";
-import { Joined } from "../../interface/models";
 
 const { height } = Dimensions.get('screen')
 
@@ -32,20 +30,32 @@ const EventScreen = () => {
         getEventByID(params.event_id);
     }, [params])
 
-
     return (
         <View>
+            {joined.joined &&
+                <Alert ContainerStyle={{
+                    position: 'absolute',
+                    zIndex: 999,
+                    top: Platform.OS === 'ios' ? 220 : 190,
+                    right: 5,
+                    borderWidth: 2.50,
+                    height: 80,
+                    justifyContent:'center',
+                    borderColor: 'red'
+                }}
+                    Message={joined.message}
+                />
+            }
             <EventHeader
                 eventName={eventInfo.event[0]?.event_title}
             />
-            <ScrollView showsVerticalScrollIndicator={false} style={{ ...styles.scrollStyle, height: height * 0.30 }}>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ ...styles.scrollStyle, height: Platform.OS === 'android' ? height * 0.26 : height * 0.33 }}>
                 <Text> {eventInfo.event[0]?.event_description} </Text>
             </ScrollView>
             <View style={styles.bottomContainer}>
                 <ButtonComponent
                     text={<Text style={styles.buttonText}>Join</Text>}
                     onPress={() => joinEvent(auth.record.user_id, params.event_id, eventInfo.event[0])}
-
                 />
                 <View style={styles.joinContainer}>
                     <Icon
@@ -55,7 +65,6 @@ const EventScreen = () => {
                     />
                     <Text style={styles.joinText}> 53 </Text>
                 </View>
-                {joined.joined && <Alert ContainerStyle={{ borderWidth: 1.10, borderColor: 'red' }} Message={joined.message} />}
             </View>
         </View>
 
