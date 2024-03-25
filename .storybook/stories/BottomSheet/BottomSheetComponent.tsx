f0ecc4db1d092752dac1f1c85dce8fc328190115
jmejiamu <jmejiamu@gmail.com>
2024-03-25 16:44:02 -0700

    fix/125-fe-fix-bottom-sheet

--- .storybook/stories/BottomSheet/BottomSheetComponent.tsx
+++ .storybook/stories/BottomSheet/BottomSheetComponent.tsx
@@ -1,21 +1,30 @@
-import React, { RefObject } from 'react';
-import { View, Text, TextInput, TextInputProps } from 'react-native';
-import BottomSheet, { BottomSheetMethods } from '@devvie/bottom-sheet';
+import React, {RefObject} from 'react';
+import {View, Text, TextInput, TextInputProps} from 'react-native';
+import BottomSheet, {BottomSheetMethods} from '@devvie/bottom-sheet';
 import ButtonComponent from '../Button/Button';
-import { styles } from './styles';
+import {styles} from './styles';
 
 interface Props {
   header: JSX.Element | string;
   footer?: JSX.Element | string;
   onHandleOpen?: () => void;
   onHandleClose: () => void;
+  onHandleCancel: () => void;
   sheetRef: RefObject<BottomSheetMethods>;
   textInputProps?: TextInputProps;
   textDescriptionInputProps?: TextInputProps;
 }
 
 const BottomSheetComponent = (props: Props) => {
-  const { header, onHandleClose, textDescriptionInputProps, footer, sheetRef, textInputProps } = props;
+  const {
+    header,
+    onHandleClose,
+    textDescriptionInputProps,
+    footer,
+    sheetRef,
+    textInputProps,
+    onHandleCancel,
+  } = props;
   const headerElem =
     typeof header === 'string' ? (
       <Text style={styles.header}>{header}</Text>
@@ -30,15 +39,34 @@ const BottomSheetComponent = (props: Props) => {
       footer
     );
   return (
-    <>
-      <BottomSheet style={styles.bottomSheetContainer} ref={sheetRef}>
-        <>{headerElem}</>
-        <TextInput style={styles.textInputStyle} {...textInputProps} />
-        <TextInput style={styles.textInputStyle} {...textDescriptionInputProps} />
-        <ButtonComponent text="Add" onPress={onHandleClose} />
-        {footerElem && <View style={styles.footerContainer}>{footerElem}</View>}
-      </BottomSheet>
-    </>
+    <BottomSheet
+      backdropMaskColor={'rgba(112, 169, 161, 0.5)'}
+      style={styles.bottomSheetContainer}
+      height={500}
+      ref={sheetRef}>
+      <>{headerElem}</>
+      <TextInput style={styles.textInputStyle} {...textInputProps} />
+      <TextInput style={styles.textInputStyle} {...textDescriptionInputProps} />
+      <View style={styles.buttonMainContainer}>
+        <View style={{flex: 1}}>
+          <ButtonComponent
+            custumStyles={styles.buttonAdd}
+            text="Add"
+            onPress={onHandleClose}
+            textStyles={styles.buttonAddText}
+          />
+        </View>
+        <View style={styles.buttonDivider} />
+        <View style={{flex: 1}}>
+          <ButtonComponent
+            custumStyles={styles.buttonCancel}
+            text="Cancel"
+            onPress={onHandleCancel}
+          />
+        </View>
+      </View>
+      {footerElem && <View style={styles.footerContainer}>{footerElem}</View>}
+    </BottomSheet>
   );
 };
 
